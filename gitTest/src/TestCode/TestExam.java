a2e1b006fa7a95c460281386bf60f06a6334293b
Haerang <harang1226@naver.com>
2023-02-21 14:08:44 +0900

    print message

--- gitTest/src/TestCode/TestExam.java
+++ gitTest/src/TestCode/TestExam.java
@@ -3,7 +3,8 @@ package TestCode;
 public class TestExam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("안녕하세요.");
+		System.out.println("테스트 중입니다.");
 
 	}
 
